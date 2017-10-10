1e0f5652dc6647eb919a72f43036736549e4d9df
Hilitrium <edfbugkiller13@gmail.com>
2017-10-09 17:44:39 -0700

    just a temp push of the 01-game math

---
+++ GameMath/GameMath/math.h
@@ -0,0 +1,33 @@
+#pragma once
+
+float sqr(float v) {
+	return v * v;
+}
+
+float dbl(float v) {
+	return v * 2;
+}
+
+float cube(float v) {
+	return v * v * v;
+}
+
+float degToRad(float deg) {
+	return deg * (3.141592654 / 180);
+}
+
+float radToDeg(float rad) {
+	return rad*(180 / 3.141592654);
+}
+
+float quad(float x) {
+	return (x * x) + (2 * x) - 7;
+}
+
+float lerp(float s, float e, float t) {
+	return s + t * (e - s);
+}
+
+float distance(float p1, float p2) {
+	(((p2 - p1) * (p2 - p1)) + ((p2 - p1)))
+}
