715a167b92c4f9db6efc6ab16500f7371b306f30
Hilitrium <edfbugkiller13@gmail.com>
2017-11-27 14:38:45 -0800

    added 1-Templates

--- GameMath/X-TestGame/main.cpp
+++ GameMath/X-TestGame/main.cpp
@@ -1,4 +1,4 @@
-
+#include "myGuy.h"
 #include "sfwdraw.h"
 #include "Transform.h"
 int main() 
@@ -18,6 +18,40 @@ int main()
 	}
 
 	sfw::termContext();
+}
+
+myGuy::myGuy(float speed, float rotSpeed, vec2 pos, vec2 dim, float anglez) {
+	Speed = Speed;
+	turnSpeed = rotSpeed;
+	myTrans.position = pos;
+	myTrans.dimension = dim;
+	myTrans.angle = anglez;
+}
+
+void myGuy::update() {//const mat3 &t) {
+	mat3 t = myTrans.getGlobalTransform();
+
+	vec2 Dir = norm((t[1].xy)) * Speed;
+
+	if (sfw::getKey('W')) {
+		myTrans.position += Dir;
+	}
+	if (sfw::getKey('S')) {
+		myTrans.position -= Dir;
+	}
+	if (sfw::getKey('A')) {
+		myTrans.angle += turnSpeed;
+	}
+	if (sfw::getKey('D')) {
+		myTrans.angle -= turnSpeed;
+	}
+}
+
+void myGuy::draw(const mat3 &t) {
+	vec2 pos = t[2].xy;
+
+	sfw::drawCircle(pos.x, pos.y, 300, 450, RED);
+	DrawMatrix(myTrans.getGlobalTransform(), 300);
 }
 
 //#include "sfwdraw.h"
--- GameMath/X-TestGame/myGuy.cpp
+++ GameMath/X-TestGame/myGuy.cpp
@@ -1,36 +1,36 @@
-#include "myGuy.h"
-#include "sfwdraw.h"
+//#include "myGuy.h"
+//#include "sfwdraw.h"
 
-myGuy::myGuy(float speed, float rotSpeed, vec2 pos, vec2 dim, float anglez) {
-	Speed = Speed;
-	turnSpeed = rotSpeed;
-	myTrans.position = pos;
-	myTrans.dimension = dim;
-	myTrans.angle = anglez;
-}
-
-void myGuy::update() {//const mat3 &t) {
-	mat3 t = myTrans.getGlobalTransform();
-
-	vec2 Dir = norm((t[1].xy)) * Speed;
-
-	if (sfw::getKey('W')) {
-		myTrans.position += Dir;
-	}
-	if (sfw::getKey('S')) {
-		myTrans.position -= Dir;
-	}
-	if (sfw::getKey('A')) {
-		myTrans.angle += turnSpeed;
-	}
-	if (sfw::getKey('D')) {
-		myTrans.angle -= turnSpeed;
-	}
-}
-
-void myGuy::draw(const mat3 &t) {
-	vec2 pos = t[2].xy;
-
-	sfw::drawCircle(pos.x, pos.y, 300, 450, BLUE);
-	DrawMatrix(myTrans.getGlobalTransform(), 20);
-}
+//myGuy::myGuy(float speed, float rotSpeed, vec2 pos, vec2 dim, float anglez) {
+//	Speed = Speed;
+//	turnSpeed = rotSpeed;
+//	myTrans.position = pos;
+//	myTrans.dimension = dim;
+//	myTrans.angle = anglez;
+//}
+//
+//void myGuy::update() {//const mat3 &t) {
+//	mat3 t = myTrans.getGlobalTransform();
+//
+//	vec2 Dir = norm((t[1].xy)) * Speed;
+//
+//	if (sfw::getKey('W')) {
+//		myTrans.position += Dir;
+//	}
+//	if (sfw::getKey('S')) {
+//		myTrans.position -= Dir;
+//	}
+//	if (sfw::getKey('A')) {
+//		myTrans.angle += turnSpeed;
+//	}
+//	if (sfw::getKey('D')) {
+//		myTrans.angle -= turnSpeed;
+//	}
+//}
+//
+//void myGuy::draw(const mat3 &t) {
+//	vec2 pos = t[2].xy;
+//
+//	sfw::drawCircle(pos.x, pos.y, 300, 450, BLUE);
+//	DrawMatrix(myTrans.getGlobalTransform(), 20);
+//}
